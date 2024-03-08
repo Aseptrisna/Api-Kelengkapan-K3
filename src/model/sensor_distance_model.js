6b3071a3e812e692e235736edc31c7b6f58b2f7e
Asep Trisna Setiawan <aseptrisna1995@gmail.com>
2024-03-08 23:21:39 +0700

    add distance

--- src/model/sensor_distance_model.js
+++ src/model/sensor_distance_model.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const DataSchema = new mongoose.Schema({
     guid: { type: String },
     value: { type: String },
+    image: { type: String },
     date: { type: String },
+    floor: { type: Number },
 }, { versionKey: false });
 
 const DataModel = mongoose.model('distance', DataSchema);
